90e6a30212aa360b0dbeb5eee4253a1b3ada5f16
janoidhari <ronakjosi8299@gmail.com>
2023-11-05 22:30:33 +0530

    second practical

---
+++ PR2.C
@@ -0,0 +1,16 @@
+// PR.2. Write a C program to print roll no, name and address.
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    clrscr();
+
+    printf("Rollno : 08 \n");
+
+    printf("Name : Ronak Joshi \n");
+
+    printf("Address : OmStack Coding Institute,Himatnagar");
+
+    getch();
+}
