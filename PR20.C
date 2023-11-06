b9956c89e7baa78535a4fc3ae65578f9bef942c0
janoidhari <ronakjosi8299@gmail.com>
2023-11-06 20:53:17 +0530

    PR20

---
+++ PR20.C
@@ -0,0 +1,22 @@
+// 20. Write a C program to display first 25 Fibonacci nos.
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a = 1, b = 1, c, i;
+    // clrscr();
+    printf("Fibonnaci series :\n");
+    printf("%d \n", a);
+    printf("%d \n", b);
+
+    for (i = 1; i <= 25; i++)
+    {
+        c = a + b;
+        printf("%d \n", c);
+        a = b;
+        b = c;
+    }
+    getch();
+}
