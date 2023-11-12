b802373a5454a53f679221cd351af3358c8f7844
janoidhari <ronakjosi8299@gmail.com>
2023-11-12 18:39:03 +0530

    PR38

---
+++ PR38.C
@@ -0,0 +1,20 @@
+// 38. Write a j program to display following output on the screen.
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n, i, j;
+    // clrscr();
+    printf("Enter Rows : ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
